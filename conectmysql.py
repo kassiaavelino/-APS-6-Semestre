ddc4acdb9b6525f44031be97a02b79a28b7addc7
Kássia <kassia11919@gmail.com>
2021-11-15 17:20:09 -0300

    trabalhando com tkinter

--- conectmysql.py
+++ conectmysql.py
@@ -1,7 +1,6 @@
 import mysql.connector
-
 # | nome_propri   |  cpf_propri     | tel_propri         | endereco_local |
-# | tamanho_local | tipo_agrotoxico | nivel_contaminacao |                |
+# | area_local | tipo_agrotoxico | nivel_contaminacao |                |
 
 db = mysql.connector.connect(
     host='127.0.0.1',
@@ -12,28 +11,66 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-def LerMinistro():
-    cursor.execute("SELECT * FROM tb_propriedades")
-
-    result = cursor.fetchall()
-
-    for x in result:
-        print(x)
-
-# LerTabela()       
-
-def LerDiretor():
-    cursor.execute("SELECT nome_propri, tel_propri, tipo_agrotoxico, nivel_contaminacao FROM tb_propriedades")
-    
-    result = cursor.fetchall()
-
-    for x in result:
-        print(x)
-
 def LerFuncionario():
     cursor.execute("SELECT nome_propri, tel_propri, nivel_contaminacao FROM tb_propriedades")
-    
     result = cursor.fetchall()
+  
+    print('┌─────────────────────────────────────────────────┐')
+    print('│   NOME  │  TELEFONE  │  NIVEL DE CONTAMINAÇÃO   │')
+    print('└─────────────────────────────────────────────────┘')
+       
+    for linha in result:
+        for elemento in linha:
+            print('',elemento,'', end='')               
+        print()
+        
+# LerFuncionario()         
+    # for linha in result:
+    #     conteudo.append(linha[0])
+    #     conteudo.append(linha[1])
+    #     conteudo.append(linha[2])
+    # print(conteudo)
+    
+    # arquivo = open('arq-aps.txt','a',encoding='utf-8')
+    
+    # arquivo.writelines(str(conteudo[0]))
+    # arquivo.writelines(str(conteudo[1]))
+    # arquivo.writelines(str(conteudo[2]))
+    # arquivo.close()
+    
+    # arquivo = open('arq-aps.txt','w', encoding='utf-8')
+    # arquivo.write(str(conteudo))
+    # arquivo.close()   
 
-    for x in result:
-        print(x)
+def LerDiretor():
+    cursor.execute("SELECT nome_propri, tel_propri, area_local, tipo_agrotoxico, nivel_contaminacao FROM tb_propriedades")
+    result = cursor.fetchall()
+    
+    print('┌────────────────────────────────────────────────────────────────────────────┐')
+    print('│  NOME  │  TELEFONE  │  AREA  │  TIPO AGROTÓXICO  │  NIVEL DE CONTAMINAÇÃO  │')
+    print('└────────────────────────────────────────────────────────────────────────────┘')
+
+    for linha in result:
+        for elemento in linha:
+            print('',elemento,'', end='')
+        print()
+
+def LerMinistro():
+    cursor.execute("SELECT * FROM tb_propriedades")
+    result = cursor.fetchall()
+    
+    print('┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐')
+    print('│      NOME      │      CPF      │      TELEFONE      │      ENDEREÇO      │      AREA      │      TIPO AGROTÓXICO      │      NIVEL DE CONTAMINAÇÃO      │')
+    print('└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘')
+
+    for linha in result:
+        for elemento in linha:
+            print('',elemento,'', end='')
+        print()
+
+
+# print(LerFuncionario(),'\n')
+
+# print(LerDiretor(),'\n')
+
+# print(LerMinistro(),'\n')
--- programafinal.py
+++ programafinal.py
@@ -5,24 +5,23 @@ import os
 import sys
 from time import sleep
 from conectmysql import *
+from tkinter import *
 
 root = tk.Tk() 
 root.withdraw()
 
+
+
 def menu_Func():
-     while(True):
+     while(True): 
           opc_menu_Func = int(input("Em que podemos ajudar? \n\n1.Mostrar dados\n2.Sair\n\nOpção: "))
           if(opc_menu_Func == 1):
                print("Dados obtidos:\n") 
                LerFuncionario()                            
                
-               sys.exit()         
-
-          elif(opc_menu_Func == 2):
-               
-               
-               sys.exit()
-          
+               sys.exit() 
+          elif(opc_menu_Func == 2):               
+               sys.exit()          
           elif(opc_menu_Func >= 3):
                print("Opção inválida. Tente novamente!")
                
@@ -54,7 +53,7 @@ def menu_Ministro():
                sys.exit()
           elif(opc_menu_Mini >= 3):
                print("Opção inválida. Tente novamente!")
- 
+
 # nivel 1 - todos tem acesso
 def funcionario():
      sleep(1)
@@ -91,11 +90,11 @@ def auth_func():
           percentage = imgcompare.is_equal("bio1.jpg", file_path)
      except:
           print("Erro na leitura biométrica,\n Tente Novamente!")
-          menu()
+          
 
      if  percentage:
           
-          funcionario() 
+          janela_func() 
  
      else:     
           print("Falha na leitura biométrica\n")
@@ -117,7 +116,7 @@ def auth_diretor():
      
      if img_aps:
           
-          diretor_div()
+          janela_diretor()
      else:     
           print("Falha na leitura biométrica\n")
           sys.exit()
@@ -131,35 +130,101 @@ def auth_ministro():
      file_path = os.path.basename(file_path)
      img_aps1 = False
 
+
      try:
           img_aps1 = imgcompare.is_equal("bio3.jpg", file_path)
      except:
           pass
 
      if img_aps1:
-
-          ministro()
+            
+          janela_ministro()
      
      else:     
           print("Falha na leitura biométrica\n")
           sys.exit()
 
-# menu para seleção do usuario desejado
-def menu():
-     while(True):
-          opcMenu = int(input("Selecione o usuário: \n\n1.Funcionário\n2.Diretor\n3.Ministro\n4.Sair\n\nOpção: "))
-          if(opcMenu == 1):
-               print("Por favor faça a leitura biométrica Funcionário X\n")               
-               auth_func()          
-          elif(opcMenu == 2):
-               print("Por favor faça a leitura biométrica Diretor")
-               auth_diretor()
-          elif(opcMenu == 3):
-               print("Por favor faça a leitura biométrica Ministro")
-               auth_ministro()
-          elif(opcMenu >= 4):
-               sys.exit()
+def janela_func():
+    jn_func = tk.Toplevel(janela)
+    labelfunc = Label (jn_func, text='Seja Bem Vindo Funcionário!')
+    labelfunc.grid(column=0, row=0)
+    
+    btn_func_dados = Button(jn_func, text="Mostrar dados", command=LerFuncionario)
+    btn_func_dados.grid(column=0, row=1)
+    btn_func_sair = Button(jn_func, text="Sair", command=quit)
+    btn_func_sair.grid(column=1, row=3)
+   
 
-print("Bem vindo!\n")
-sleep(1)
-menu()
+def janela_diretor():
+    jn_di = tk.Toplevel(janela)
+    labelfunc = Label (jn_di, text='Seja Bem Vindo Diretor!')
+    labelfunc.grid(column=0, row=0)
+    
+    btn_func_dados = Button(jn_di, text="Mostrar dados", command=LerDiretor)
+    btn_func_dados.grid(column=0, row=1)
+    btn_func_sair = Button(jn_di, text="Sair", command=quit)
+    btn_func_sair.grid(column=1, row=3)
+
+def janela_ministro():
+    jn_min = tk.Toplevel(janela)
+    labelfunc = Label (jn_min, text='Seja Bem Vindo Ministro!')
+    labelfunc.grid(column=0, row=0)
+    
+    btn_func_dados = Button(jn_min, text="Mostrar dados", command=LerMinistro)
+    btn_func_dados.grid(column=0, row=1)
+    btn_func_sair = Button(jn_min, text="Sair", command=quit)
+    btn_func_sair.grid(column=1, row=3)   
+    
+janela=Tk()
+janela.title("Programa de reconhecimento biometrico")
+janela.geometry('460x150')
+infoJanela = Label (janela, text = "Bem vindo ao Programa de Biometria", font='broadway')
+infoJanela.grid(column=1, row=0)
+
+infoJanela1 = Label (janela, text = "Por favor, escolha a sua opção de Login", font='georgia')
+infoJanela1.grid(column=1, row=1)
+
+botao1 = Button(janela, text="Funcionario", command=auth_func)
+botao1.grid(column=0, row=2)
+
+botao2 = Button(janela, text="Diretor", command=auth_diretor)
+botao2.grid(column=1, row=2)
+
+botao3 = Button(janela, text="Ministro", command=auth_ministro)
+botao3.grid(column=2, row=2)
+
+lbl_space = Label (janela, text = "")
+lbl_space.grid(column=1, row=3)
+
+botao4 = Button(janela, text="Sair", command=quit)
+botao4.grid(column=2, row=4)
+
+janela.mainloop()
+
+janela1=Tk()
+janela1.title("biometria")
+infoJanela1 = Label (janela1, text = "Bem vindo ao Programa de Biometria")
+infoJanela1.grid(column=1, row=0)
+janela.mainloop()
+
+
+
+# menu para seleção do usuario desejado
+# def menu():
+#      while(True):
+#           opcMenu = int(input("Selecione o usuário: \n\n1.Funcionário\n2.Diretor\n3.Ministro\n4.Sair\n\nOpção: "))
+#           if(opcMenu == botao1):
+#                print("Por favor faça a leitura biométrica Funcionário X\n")               
+#                auth_func()          
+#           elif(opcMenu == 2):
+#                print("Por favor faça a leitura biométrica Diretor")
+#                auth_diretor()
+#           elif(opcMenu == 3):
+#                print("Por favor faça a leitura biométrica Ministro")
+#                auth_ministro()
+#           elif(opcMenu >= 4):
+#                sys.exit()
+
+# print("Bem vindo!\n")
+# sleep(1)
+# menu()
